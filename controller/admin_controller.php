1c46a185aefc7dee01579f00679684a26155d43d
david63 <david.wood@pear-tree.org.uk>
2019-05-19 13:37:25 +0100

    Fixes

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -10,7 +10,6 @@
 namespace david63\resetregdate\controller;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
 use phpbb\db\driver\driver_interface;
 use phpbb\request\request;
 use phpbb\template\template;
